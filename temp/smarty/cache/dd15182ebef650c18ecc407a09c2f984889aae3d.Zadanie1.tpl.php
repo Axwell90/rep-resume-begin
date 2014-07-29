d387b5528414e655c050640e3404f1c333497a1e
Axwell90 aaa <Axwell90@yandex.ru>
2014-07-29 16:08:57 +0400

    smarty_test

---
+++ temp/smarty/cache/dd15182ebef650c18ecc407a09c2f984889aae3d.Zadanie1.tpl.php
@@ -0,0 +1,62 @@
+<?php /*%%SmartyHeaderCode:131435421653ac50771112f4-79499791%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    'dd15182ebef650c18ecc407a09c2f984889aae3d' => 
+    array (
+      0 => '/srv/http/temp/smarty/templates/Zadanie1.tpl',
+      1 => 1403800942,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '131435421653ac50771112f4-79499791',
+  'variables' => 
+  array (
+    'results' => 0,
+    'article_id' => 0,
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.18',
+  'unifunc' => 'content_53ac50771a7245_22682234',
+  'cache_lifetime' => 120,
+),true); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_53ac50771a7245_22682234')) {function content_53ac50771a7245_22682234($_smarty_tpl) {?><!DOCTYPE html>
+<html>
+ <head>
+   <meta http-equiv="content-type" content="text/html" charset="utf-8">
+      <title>SmartyTest</title>
+ <script type="text/javascript" src="../js/jquery.js"></script>
+ <script type="text/javascript" src="../js/Zadanie1.js"></script>
+
+</head>
+<body style="background-color:gray" text="#000080">
+
+<form action="Zadanie1.php" method="post">
+   <h1 style="text-align:center">
+  E-mail <br />
+  <input type="text" name="mail">
+
+
+         <input type="submit" value="ADD RECORD">
+
+</h1>
+</form>
+	
+
+
+	
+	 	 
+<div id="47" class="points" title="qew" style="position: absolute; width: 15px; height: 15px; background-color: #00FF00; top: 93px; left: 69px;"></div>
+
+			 
+			 
+
+
+
+
+<p>5 67  </p>
+<div id="idd" class="qwe">afghf  [HTML is replaced when successful.]</div>
+</body></html>
+
+
+<?php }} ?>
---
+++ temp/smarty/templates/zadanie.tpl
@@ -0,0 +1,21 @@
+<html><head>
+   <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+
+   <title>Zadanie</title>
+</head><body>
+
+	<h1 style="text-align:center;"style="background-color:gray;">E-mail: $mail<br />
+	<form action="zadanie.php" method="post"><pre>
+		
+		<input type="text" mail="$mail" />
+		<input type="submit" value="Отправить"/>
+	</pre></form></h1>
+
+<div title="hui@yandex.ru" style="position: absolute; width: 5px; height: 5px; background-color: #00FF00; top: 250px; left: 400px;" />
+<div title="2222@yandex.ru" style="position: absolute; width: 5px; height: 5px; background-color: #00FF00; top: 250px; left: 300px;" />
+
+
+
+</body></html>
+
+
